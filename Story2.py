2e409802cbede73ed1cba7986186a35d6b469a08
Noah Clark <noah.clark@kearsarge.org>
2023-11-01 07:43:30 -0400

    Edited story 2, added some \n charictors

--- Story2.py
+++ Story2.py
@@ -22,14 +22,16 @@ def Story2(debug=False):
     clear()
     cuss =   getSwear("swear please > ", debug)
     clear()
-    letters = "One fine evening, " + person 
+    
+    
+    letters = "One fine evening, " + person
     letters += " had a craving to " + verb + "\n" 
-    letters += " until the sun began to rise. " + person
-    letters += " would do it at the " + event
-    letters += " at " + place + "\n"
-    letters += ", and he would bring a " + sillyThing 
-    letters += ". It was " + color + "."
-    letters += " When " + person + " got to the " + event + " at \n" + place
-    letters += ", the " + monster + " at the door called him a " + cuss + ". \n"
-    letters += person + " went home disappointed."
+    letters += "until the sun began to rise. " + person+"\n"
+    letters += "would do it at the " + event
+    letters += " at " + place + ",\n"
+    letters += "and he would bring a " + sillyThing +". "
+    letters += "It was " + color + ".\n"
+    letters += "When " + person + " got to the " + event + " at " + place+",\n"
+    letters += "the " + monster + " at the door called him a " + cuss + ". \n"
+    letters += person + "went home disappointed.\n"
     return letters
