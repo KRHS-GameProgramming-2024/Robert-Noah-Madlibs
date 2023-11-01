09c36b62701739c83b4d30049ecf075a02bc999d
Noah Clark <noah.clark@kearsarge.org>
2023-11-01 09:03:59 -0400

    fixed issues with consistancey between functions

--- madlibsMain.py
+++ madlibsMain.py
@@ -29,8 +29,8 @@ def Madlibs(debug = False):
             print(Story2(debug))
         elif option == "3":
             print(Story3(debug))
-        elif option == "4":
-            print(Story4(debug))
+        #elif option == "4":
+        #    print(Story4SECRET(debug))
             
         input("Press enter to continue")
         clear()
